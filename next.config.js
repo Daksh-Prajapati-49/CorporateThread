14de8ab5ac5acd6bb845f0f2acbe8b484a9d86f1
Daksh Prajapati <89261276+Daksh-Prajapati-49@users.noreply.github.com>
2024-04-07 21:03:08 +0530

    done

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: 'out',
 }
 
 module.exports = nextConfig
